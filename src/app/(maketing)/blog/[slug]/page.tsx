cc31a1bad7501151e38c9df8e682a6e74845c097
GL0riFy <160113791+Siyyhoi@users.noreply.github.com>
2025-06-18 10:55:14 +0700

    Update page.tsx

--- src/app/(maketing)/blog/[slug]/page.tsx
+++ src/app/(maketing)/blog/[slug]/page.tsx
@@ -1,6 +1,12 @@
-export default async function BlogPost({ params }) {
-  const { slug } = await params; 
-  
+interface BlogPostPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function BlogPost({ params }: BlogPostPageProps) {
+  const { slug } = params;
+
   return (
     <div>
       <h1>บทความ: {slug}</h1>
